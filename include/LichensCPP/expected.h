a8098390b431b2ab3846362e3f434ff3507264c2
Jérôme Godbout <godboutj@lichens.ai>
2025-11-05 12:14:43 -0500

    Avoid namespace nesting with include for expected

--- include/LichensCPP/expected.h
+++ include/LichensCPP/expected.h
@@ -9,24 +9,30 @@
 
 #pragma once
 
+#include <version>
+
+#if __cpp_lib_expected >= 202202L
+    #include <expected>
+#else
+    #include "LichensCPP/expected.hpp"
+#endif
+
 namespace LichensCPP 
 {
 
 // Use std::expected if available, otherwise fall back to tl::expected as shim
 #if __cpp_lib_expected >= 202202L
-    #include <expected>
-        template <class T, class E>
-        using expected = std::expected<T, E>;
+    template <class T, class E>
+    using expected = std::expected<T, E>;
 
-        template <class E>
-        using unexpected = std::unexpected<E>;
+    template <class E>
+    using unexpected = std::unexpected<E>;
 #else
-    #include "LichensCPP/expected.hpp"
-        template <class T, class E>
-        using expected = tl::expected<T, E>;
+    template <class T, class E>
+    using expected = tl::expected<T, E>;
 
-        template <class E>
-        using unexpected = tl::unexpected<E>;
+    template <class E>
+    using unexpected = tl::unexpected<E>;
 #endif
 
 } // namespace LichensCPP
